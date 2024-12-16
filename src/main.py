a7c5aafaa6519f06a410d1aac6e9303ab695cf33
AlexArdelean21 <ardeleanalexandru22@stud.ase.ro>
2024-12-16 14:21:46 +0200

    Final commit

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ import os
 os.system("python data_preprocessing.py")
 os.system("python eda_analysis.py")
 os.system("python pca_analysis.py")
-#os.system("python cluster_analysis.py")
+os.system("python cluster_analysis.py")
 
 print("Workflow completed! Check the 'output/' folder for results.")
